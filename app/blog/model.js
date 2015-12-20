a2e2411832254a6a7ecc95e3b656839780b259b2
Allison Sichling <saphron.scul@gmail.com>
2015-12-20 16:33:31 -0500

    Created template to display single blog and Edit Blog, Author Name will display with Blog instead of Author ID, Added Logout

--- app/blog/model.js
+++ app/blog/model.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   _id: DS.attr('string'),
   title: DS.attr('string'),
-  description: DS.attr('string'),
-  author: DS.attr('string'),
+  content: DS.attr('string'),
+  authorName: DS.attr('string'),
   // posts: DS.hasMany('post', {async: true})
 });
--- app/components/each-blog/component.js
+++ app/components/each-blog/component.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditable: false,
+  // doubleClick: function(){
+  //   this.toggleProperty('isEditable');
+  // },
+  actions: {
+    toggleEditable: function(){
+      this.toggleProperty('isEditable');
+    },
+    confirmEdit: function(blog){
+      console.log("editing a blog");
+      console.log(blog);
+
+      Ember.$.ajax({
+        url: "http://localhost:3000/blogs/"+ blog.id,
+        method: "put",
+        contentType: "application/json",
+        xhrFields: { withCredentials: true },
+        data: JSON.stringify(blog)
+      });
+
+      this.toggleProperty('isEditable');
+    }
+    // destroyItem: function(){
+    //   this.sendAction('routeDestroyBlog', this.get('blog'));
+    // },
+  }
 });
