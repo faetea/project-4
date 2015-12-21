cbe8740c70fafdc28c75939064b1d8fce46d7cb1
Allison Sichling <saphron.scul@gmail.com>
2015-12-20 20:16:54 -0500

    second try deploying ember to gh-pages

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   // host: 'http://localhost:3000',
-  host: 'https://desolate-headland-9492.herokuapp.com/',
+  host: 'https://desolate-headland-9492.herokuapp.com',
   primaryKey: '_id'
 });
--- app/blog/adapter.js
+++ app/blog/adapter.js
@@ -2,6 +2,6 @@ import ApplicationAdapter from '../application/adapter';
 
 export default ApplicationAdapter.extend({
   // host: 'http://localhost:3000',
-  host: 'https://desolate-headland-9492.herokuapp.com/',
+  host: 'https://desolate-headland-9492.herokuapp.com',
   primaryKey: '_id'
 });
